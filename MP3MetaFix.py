c6a9a43db4ad910560e0b8c07e86321abdf868cc
Nic McCool <nicmccool@gmail.com>
2020-04-07 13:54:25 -0400

    Fixed some error looping

---
+++ MP3MetaFix.py
@@ -0,0 +1,78 @@
+import eyed3, os
+
+testDirectory = input("Enter the directory like this G:\\My Drive\\PlexLink\\Audiobooks:")
+logFile = open("errors.log", "w")
+
+def editMeta(topFolder):
+    for root, dirs, files in os.walk(testDirectory + "\\" + topFolder):
+        if not files:
+            continue
+        for f in files:
+            if f.lower().endswith(('.mp3')):
+                fullFile = os.path.join(root,f)
+                changeFile = eyed3.load(fullFile)
+                print(fullFile)
+                changeCheck = 1
+
+                try:
+                    try:
+                        print("OLD = " + str(changeFile.tag.album_artist))
+                    except AttributeError:
+                        print("OLD = Blank Value")   
+
+                    try:
+                        if changeFile.tag.album_artist != topFolder:
+                            print("Album Artist doesn't match")
+                            changeFile.tag.album_artist = topFolder
+                            changeCheck += 1
+                        else:
+                            print("Album Artist matches.")
+                    except AttributeError:
+                            print("Album Artist is NONE")
+                            try:
+                                changeFile.tag.album_artist = topFolder
+                            except AttributeError:
+                                "Couldn't change the Album Artist."
+                            changeCheck += 1
+
+                    try:
+                        if changeFile.tag.artist != topFolder:
+                            print("Artist doesn't match")
+                            changeFile.tag.artist = topFolder
+                            changeCheck += 1
+                        else:
+                            print("Artist matches.")
+                    except AttributeError:
+                            print("Artist is NONE")
+                            try:
+                                changeFile.tag.artist = topFolder
+                            except AttributeError:
+                                "Couldn't change the Artist."
+                            changeCheck += 1
+
+                    if changeCheck > 1:
+                        changeFile.tag.save()
+                        print("NEW AA = " + str(changeFile.tag.album_artist))
+                        print("NEW A = " + str(changeFile.tag.artist))
+                    else:
+                        print("No changes needed for " + f)
+                except Exception:
+                    logFile.write(fullFile)
+                    logFile.close
+
+def subDirOne(root_dir):
+    entries = os.listdir(root_dir)
+    all_dirs = []
+
+    for entry in entries:
+        path = os.path.join(root_dir, entry)
+        if os.path.isdir(path):
+            all_dirs.append(entry)
+
+    return all_dirs
+
+output_dirs = subDirOne(testDirectory)
+print(output_dirs)
+
+for item in sorted(output_dirs):
+    editMeta(item)
